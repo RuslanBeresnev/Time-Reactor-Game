44ca1ae48473556e7558499e94da461a3fb4e960
Руслан Береснев <st098265@student.spbu.ru>
2022-07-08 11:16:06 +0300

    Изменено добавление этажа к пройденным

--- Assets/Scripts/EnemyAppearance.cs
+++ Assets/Scripts/EnemyAppearance.cs
@@ -14,6 +14,8 @@ public class EnemyAppearance : MonoBehaviour
         {
             if (PlayerController.ZAxisDirection == PlayerController.AxisDirection.Forward && !GameProperties.PassedFloors.Contains(GameProperties.FloorNumber))
             {
+                GameProperties.PassedFloors.Add(GameProperties.FloorNumber);
+
                 var random = new System.Random();
                 float generatedFloat = (float)random.NextDouble();
                 if (generatedFloat < GameProperties.AppearanceChance)
