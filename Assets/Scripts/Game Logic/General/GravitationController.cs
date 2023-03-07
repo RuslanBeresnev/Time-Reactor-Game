5b691be4108b00162c6d31cd703e5f7722e4f805
RuslanBeresnev <rusber03102003@gmail.com>
2023-03-07 11:08:43 +0300

    Добавлена настраиваемая механика гравитации для объектов с компонентом Rigidbody

---
+++ Assets/Scripts/Game Logic/General/GravitationController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+/// <summary>
+/// Реализация механики гравитации для объектов с Rigidbody
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class GravitationController : MonoBehaviour
+{
+    [SerializeField] private float gravityScale = 1f;
+
+    private Rigidbody rigidBody;
+
+    /// <summary>
+    /// Мировое ускорение свободного падения
+    /// </summary>
+    public static float GlobalGravity => -9.81f;
+
+    /// <summary>
+    /// Множитель гравитации для каждого объекта
+    /// </summary>
+    public float GravityScale
+    {
+        get { return gravityScale; }
+        set { gravityScale = value; }
+    }
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 gravityVector = GlobalGravity * GravityScale * Vector3.up;
+        rigidBody.AddForce(gravityVector, ForceMode.Acceleration);
+    }
+}
