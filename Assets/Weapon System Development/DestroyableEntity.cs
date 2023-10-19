5e9b2d06466f2b19d70f042de8b4c9dda72aadea
YaSaDog <aleyavoff@gmail.com>
2023-10-19 20:56:34 +0300

    Тестовая сцена, стрельба через Raycast

---
+++ Assets/Weapon System Development/DestroyableEntity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableEntity : Entity
+{
+    public override string ObjectInfoHeader { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public override Color ObjectInfoHeaderColor { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public override string[,] ObjectInfoParameters { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+}
