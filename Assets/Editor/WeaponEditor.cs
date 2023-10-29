8a9a6d94d12472e9eef0de33c0cacd7afc218191
YaSaDog <aleyavoff@gmail.com>
2023-10-29 21:39:31 +0300

    создан Custom Editor для Weapon.cs

---
+++ Assets/Editor/WeaponEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Weapon))]
+public class WeaponEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Weapon weapon = (Weapon)target;
+
+
+    }
+}
