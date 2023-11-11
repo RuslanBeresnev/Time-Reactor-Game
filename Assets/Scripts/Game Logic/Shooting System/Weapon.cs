d90646c91941f258595c0f29bedcfe7cd7c01eab
YaSaDog <aleyavoff@gmail.com>
2023-11-11 14:31:58 +0300

    Исправлен отступ барьера

--- Assets/Scripts/Game Logic/Shooting System/Weapon.cs
+++ Assets/Scripts/Game Logic/Shooting System/Weapon.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.Assertions.Must;
 
 /// <summary>
 /// Вид оружия
@@ -456,10 +457,10 @@ public class Weapon : ObjectWithInformation, ISerializationCallbackReceiver
         float yRotation = gameObject.transform.rotation.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, yRotation, 0);
 
-        //Отступ по оси OY, чтобы барьер ставился точно на пол
-        float yOffset = wallPrefab.transform.localScale.y / 2.0f;
-        Vector3 position = new Vector3(hit.point.x, hit.point.y + yOffset, hit.point.z);
+        //Отступ, чтобы барьер ставился точно на поверхность
+        float yOffset = wallPrefab.transform.localScale.y / 2.0f * hit.normal.y;
         
+        Vector3 position = new Vector3(hit.point.x, hit.point.y + yOffset, hit.point.z);
         var wallGO = Instantiate(wallPrefab, position, rotation);
     } 
 
