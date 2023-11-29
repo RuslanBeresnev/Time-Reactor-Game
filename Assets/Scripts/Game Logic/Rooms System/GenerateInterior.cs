03d4cf6dd53ab60d7a2ee755cf082edcd180caca
PolinaSavelyeva <lissava00@gmail.com>
2023-11-29 18:20:05 +0300

    Форматирован код скрипта для генерации интерьера

--- Assets/Scripts/Game Logic/Rooms System/GenerateInterior.cs
+++ Assets/Scripts/Game Logic/Rooms System/GenerateInterior.cs
@@ -2,80 +2,73 @@ using System;
 using RoomInteriorGenerator;
 using System.Linq;
 using Microsoft.FSharp.Core;
-using UnityEditor.Experimental.RestService;
 using UnityEngine;
-using UnityEngine.UIElements;
 
+/// <summary>
+/// Процедурная генерация интерьера в комнатах
+/// </summary>
 public class GenerateInterior : MonoBehaviour
 {
     [SerializeField] private int roomLength;
     [SerializeField] private int roomWidth;
     [SerializeField] private DataTableRowWrapper[] dataTable;
     [SerializeField] private int maximumAmountOfObjects;
-    private int floorNumber = GameProperties.FloorNumber;
-    private static Quaternion quaternion;
+    private readonly int floorNumber = GameProperties.FloorNumber;
 
     private void Start()
     {
-        var rows = dataTable.Select(row => row.ToDataTableRow()).ToArray();
-        var dataTableFs = new DataTable.DataTable<GameObject>(rows);
-        var room = new Room<GameObject>(roomLength, roomWidth, floorNumber, dataTableFs);
-        var tupleFunc =
-            FuncConvert.ToFSharpFunc<Tuple<DataTable.DataTableRow<GameObject>, DataTable.ObjectVariant<GameObject>, int, int>>(
+        var dataTableRows = dataTable.Select(row => row.ToDataTableRow()).ToArray();
+        var dataTableFs = new DataTable.DataTable<GameObject>(dataTableRows);
+
+        // Приведение функции размещения к правильному F# типу
+        var tuplePlacementFunction =
+            FuncConvert
+                .ToFSharpFunc<Tuple<DataTable.DataTableRow<GameObject>, DataTable.ObjectVariant<GameObject>, int, int>>(
                     t => PlaceObject(t.Item1, t.Item2, t.Item3, t.Item4));
-        var func = FuncConvert.FuncFromTupled(tupleFunc);
+        var placementFunction = FuncConvert.FuncFromTupled(tuplePlacementFunction);
 
-        room.GenerateInterior(maximumAmountOfObjects, func);
-
+        var room = new Room<GameObject>(roomLength, roomWidth, floorNumber, dataTableFs);
+        room.GenerateInterior(maximumAmountOfObjects, placementFunction);
     }
+
+    /// <summary>
+    /// Приведение правила расположения объекта к подходящему углу поворота по оси Y
+    /// </summary>
+    private static Quaternion ToQuaternion(DataTable.Rule placementRule) =>
+        placementRule.ToString() switch
+        {
+            "Node None" => Quaternion.Euler(0, 180, 0),
+            "Node AgainstTheBottomWall" => Quaternion.Euler(0, 0, 0),
+            "Node AgainstTheTopWall" => Quaternion.Euler(0, 180, 0),
+            "Node AgainstTheRightWall" => Quaternion.Euler(0, 270, 0),
+            "Node AgainstTheLeftWall" => Quaternion.Euler(0, 90, 0),
+            "Node InTheCorner" => Quaternion.Euler(0, 180, 0),
+            "Leaf LeftTo" => Quaternion.Euler(0, 270, 0),
+            "Leaf RightTo" => Quaternion.Euler(0, 90, 0),
+            "Leaf Behind" => Quaternion.Euler(0, 0, 0),
+            "Leaf InFrontOf" => Quaternion.Euler(0, 180, 0),
+            "Leaf Anywhere" => Quaternion.Euler(0, 0, 0),
+            _ => throw new InvalidOperationException($"Unknown placementRule value: {placementRule}")
+        };
+
+    /// <summary>
+    /// Функция размещения объекта в комнате
+    /// </summary>
     private void PlaceObject(DataTable.DataTableRow<GameObject> objectToPlace,
         DataTable.ObjectVariant<GameObject> objectToPlaceVariant, int rowIndex, int columnIndex)
     {
         var prefab = objectToPlaceVariant.Variant;
         var placementRule = objectToPlace.PlacementRule;
-        switch (placementRule.ToString())
-        {
-            case "Node None":
-                quaternion = Quaternion.Euler(0, 180, 0);
-                break;
-            case "Node AgainstTheBottomWall":
-                quaternion = Quaternion.Euler(0, 0, 0);
-                break;
-            case "Node AgainstTheTopWall":
-                quaternion = Quaternion.Euler(0, 180, 0);
-                break;
-            case "Node AgainstTheRightWall":
-                quaternion = Quaternion.Euler(0, 270, 0);
-                break;
-            case "Node AgainstTheLeftWall":
-                quaternion = Quaternion.Euler(0, 90, 0);
-                break;
-            case "Node InTheCorner":
-                quaternion = Quaternion.Euler(0, 180, 0);
-                break;
-            case "Leaf LeftTo":
-                quaternion = Quaternion.Euler(0, 270, 0);
-                break;
-            case "Leaf RightTo":
-                quaternion = Quaternion.Euler(0, 90, 0);
-                break;
-            case "Leaf Behind":
-                quaternion = Quaternion.Euler(0, 0, 0);
-                break;
-            case "Leaf InFrontOf":
-                quaternion = Quaternion.Euler(0, 180, 0);
-                break;
-            case "Leaf Anywhere":
-                quaternion = Quaternion.Euler(0, 0, 0);
-                break;
-            default:
-                throw new InvalidOperationException($"Unknown placementRule value: {placementRule}");
-        }
 
-        var delta = new Vector3(rowIndex + transform.position.x, transform.position.y, columnIndex + transform.position.z);
-        Instantiate(prefab, delta, quaternion, transform);
+        var parentPosition = transform.position;
+        var position = new Vector3(rowIndex + parentPosition.x, parentPosition.y, columnIndex + parentPosition.z);
+
+        Instantiate(prefab, position, ToQuaternion(placementRule), transform);
     }
 
+    /// <summary>
+    /// Обертка для отображения DataTable в инспекторе
+    /// </summary>
     [Serializable]
     public class DataTableRowWrapper
     {
@@ -93,62 +86,51 @@ public class GenerateInterior : MonoBehaviour
             InFrontOfParent,
             AnywhereNearParent
         }
+
         [SerializeField] private string name;
         [SerializeField] private ObjectVariantWrapper[] variants;
         [SerializeField] private PlacementRuleWrapper placementRule;
-        [SerializeField] private DataTableRowWrapper[] leafs;
+        [SerializeField] private DataTableRowWrapper[] leafDataTable;
 
-        private DataTable.Rule ParseRule()
-        {
-            switch (placementRule)
+        /// <summary>
+        /// Приведение обертки PlacementRuleWrapper к библиотечному типу PlacementRule
+        /// </summary>
+        private DataTable.Rule ToPlacementRule(PlacementRuleWrapper rule) =>
+            rule switch
             {
-                case PlacementRuleWrapper.WithoutRule:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.None);
+                PlacementRuleWrapper.WithoutRule => DataTable.Rule.NewNode(DataTable.NodePlacementRule.None),
+                PlacementRuleWrapper.AgainstTheBottomWall => DataTable.Rule.NewNode(DataTable.NodePlacementRule
+                    .AgainstTheBottomWall),
+                PlacementRuleWrapper.AgainstTheTopWall => DataTable.Rule.NewNode(DataTable.NodePlacementRule
+                    .AgainstTheTopWall),
+                PlacementRuleWrapper.AgainstTheRightWall => DataTable.Rule.NewNode(DataTable.NodePlacementRule
+                    .AgainstTheRightWall),
+                PlacementRuleWrapper.AgainstTheLeftWall => DataTable.Rule.NewNode(DataTable.NodePlacementRule
+                    .AgainstTheLeftWall),
+                PlacementRuleWrapper.InTheCorner => DataTable.Rule.NewNode(DataTable.NodePlacementRule.InTheCorner),
+                PlacementRuleWrapper.LeftToParent => DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.LeftTo),
+                PlacementRuleWrapper.RightToParent => DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.RightTo),
+                PlacementRuleWrapper.BehindParent => DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.Behind),
+                PlacementRuleWrapper.InFrontOfParent => DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.InFrontOf),
+                PlacementRuleWrapper.AnywhereNearParent => DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.Anywhere),
+                _ => throw new InvalidOperationException($"Unknown placementRule value: {rule}")
+            };
 
-                case PlacementRuleWrapper.AgainstTheBottomWall:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.AgainstTheBottomWall);
-
-                case PlacementRuleWrapper.AgainstTheTopWall:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.AgainstTheTopWall);
-
-                case PlacementRuleWrapper.AgainstTheRightWall:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.AgainstTheRightWall);
-
-                case PlacementRuleWrapper.AgainstTheLeftWall:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.AgainstTheLeftWall);
-
-                case PlacementRuleWrapper.InTheCorner:
-                    return DataTable.Rule.NewNode(DataTable.NodePlacementRule.InTheCorner);
-
-                case PlacementRuleWrapper.LeftToParent:
-                    return DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.LeftTo);
-
-                case PlacementRuleWrapper.RightToParent:
-                    return DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.RightTo);
-
-                case PlacementRuleWrapper.BehindParent:
-                    return DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.Behind);
-
-                case PlacementRuleWrapper.InFrontOfParent:
-                    return DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.InFrontOf);
-
-                case PlacementRuleWrapper.AnywhereNearParent:
-                    return DataTable.Rule.NewLeaf(DataTable.LeafPlacementRule.Anywhere);
-                default:
-                    throw new InvalidOperationException($"Unknown placementRule value: {placementRule}");
-            }
-        }
-
+        /// <summary>
+        /// Приведение обертки DataTableRowWrapper к библиотечному типу DataTableRow
+        /// </summary>
         public DataTable.DataTableRow<GameObject> ToDataTableRow()
         {
             var variantsArray = variants.Select(variant => variant.ToObjectVariant()).ToArray();
-            var leafsDataTable = leafs.Select(row => row.ToDataTableRow()).ToArray();
-            var dataTableRow = new DataTable.DataTableRow<GameObject>(name, variantsArray, ParseRule(), leafsDataTable);
-            return dataTableRow;
+            var leafsDataTable = leafDataTable.Select(row => row.ToDataTableRow()).ToArray();
+            return new DataTable.DataTableRow<GameObject>(name, variantsArray, ToPlacementRule(placementRule),
+                leafsDataTable);
         }
-
     }
 
+    /// <summary>
+    /// Обертка для отображения ObjectVariant в инспекторе
+    /// </summary>
     [Serializable]
     public class ObjectVariantWrapper
     {
@@ -158,12 +140,13 @@ public class GenerateInterior : MonoBehaviour
         [SerializeField] private int freeCellsOnTop;
         [SerializeField] private int freeCellsOnBottom;
 
+        /// <summary>
+        /// Приведение обертки ObjectVariantWrapper к библиотечному типу ObjectVariant
+        /// </summary>
         public DataTable.ObjectVariant<GameObject> ToObjectVariant()
         {
-            var objectVariant = new DataTable.ObjectVariant<GameObject>(variant, freeCellsOnLeft, freeCellsOnRight, freeCellsOnTop, freeCellsOnBottom);
-            return objectVariant;
+            return new DataTable.ObjectVariant<GameObject>(variant, freeCellsOnLeft, freeCellsOnRight, freeCellsOnTop,
+                freeCellsOnBottom);
         }
-
     }
 }
-
